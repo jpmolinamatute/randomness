c8376ee0f4a2c47a80141fa071495f81cf5e9b4b
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2024-12-14 21:53:32 -0500

    cleaning code

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 #! /usr/bin/env python
 import sys
 
+from dotenv import load_dotenv
+
 from src.auth import SpotifyAuth
 from src.client import SpotifyClient
+from src.db_client import DBClient
 from src.randomness import Randomness
 
 
 def main() -> None:
+    load_dotenv()
+    my_mongo = DBClient()
+    if my_mongo.check_connection():
+        raise ConnectionError("MongoDB is not available")
     sp_auth = SpotifyAuth()
-    sp_client = SpotifyClient(sp_auth)
-    randomness = Randomness()
+    sp_client = SpotifyClient(sp_auth, my_mongo)
+    randomness = Randomness(my_mongo)
     # sp_client.get_all_liked_tracks()
     randomness.get_random_item("track", 300)
     # print(randomness.get_random_item("artist", 5))
