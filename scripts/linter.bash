8541eb174b0ef95f151917b4c6396be67c258e3c
Juan Pablo Molina Matute <jpmolinamatute@users.noreply.github.com>
2025-11-19 20:45:41 -0600

    working on scripts (#7)

---
+++ scripts/linter.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+ROOT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/.." && pwd)"
+
+
+log_info() { echo "INFO: $*"; }
+log_error() { echo "ERROR: $*" >&2; }
+
+main() {
+    local pyproject_path="${ROOT_DIR}/pyproject.toml"
+    # export PYTHONPATH="${ROOT_DIR}/spotify"
+    # shellcheck source=../.venv/bin/activate
+    source "${ROOT_DIR}/.venv/bin/activate"
+    log_info "Running isort..."
+    isort --settings-file "${pyproject_path}" "${ROOT_DIR}/spotify" "${ROOT_DIR}/tests"
+    log_info "Running black..."
+    black --config "${pyproject_path}" "${ROOT_DIR}/spotify" "${ROOT_DIR}/tests"
+    log_info "Running mypy..."
+    mypy --config-file "${pyproject_path}" "${ROOT_DIR}/spotify" "${ROOT_DIR}/tests"
+    log_info "Running pylint..."
+    pylint --rcfile "${pyproject_path}" "${ROOT_DIR}/spotify" "${ROOT_DIR}/tests"
+}
+
+main "$@"
