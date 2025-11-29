e1614776eb22f4cf50c5daa9493136531c86f21e
Juan Pablo Molina Matute <jpmolinamatute@users.noreply.github.com>
2025-11-28 19:02:55 -0600

    making randomness async (#12)

--- scripts/linter.bash
+++ scripts/linter.bash
@@ -8,6 +8,13 @@ ROOT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/.." && pwd)"
 log_info() { echo "INFO: $*"; }
 log_error() { echo "ERROR: $*" >&2; }
 
+
+run_pytest() {
+    local pyproject_path="${ROOT_DIR}/pyproject.toml"
+    log_info "running python tests..."
+    pytest --config-file "${pyproject_path}"
+}
+
 main() {
     local pyproject_path="${ROOT_DIR}/pyproject.toml"
     # export PYTHONPATH="${ROOT_DIR}/spotify"
@@ -21,6 +28,7 @@ main() {
     mypy --config-file "${pyproject_path}" "${ROOT_DIR}/spotify" "${ROOT_DIR}/tests"
     log_info "Running pylint..."
     pylint --rcfile "${pyproject_path}" "${ROOT_DIR}/spotify" "${ROOT_DIR}/tests"
+    run_pytest
 }
 
 main "$@"
