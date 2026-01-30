36c2ffb7043dce500a198e962d5b9b349d8fc6fd
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2026-01-30 17:02:46 -0500

    implementing PR feedback

--- scripts/linter.bash
+++ scripts/linter.bash
@@ -12,20 +12,17 @@ log_error() { echo "ERROR: $*" >&2; }
 run_pytest() {
     local pyproject_path="${ROOT_DIR}/pyproject.toml"
     log_info "running python tests..."
-    pytest --config-file "${pyproject_path}"
+    uv run pytest --config-file "${pyproject_path}"
 }
 
 main() {
     local pyproject_path="${ROOT_DIR}/pyproject.toml"
-    # export PYTHONPATH="${ROOT_DIR}/spotify"
-    # shellcheck source=../.venv/bin/activate
-    source "${ROOT_DIR}/.venv/bin/activate"
     log_info "Running mypy..."
-    mypy --config-file "${pyproject_path}" "${ROOT_DIR}/spotify" "${ROOT_DIR}/tests"
+    uv run ty check
     log_info "Running ruff check --fix..."
-    ruff check --fix --config "${pyproject_path}" "${ROOT_DIR}/spotify" "${ROOT_DIR}/tests"
+    uv run ruff check --fix --config "${pyproject_path}"
     log_info "Running ruff format..."
-    ruff format --config "${pyproject_path}" "${ROOT_DIR}/spotify" "${ROOT_DIR}/tests"
+    uv run ruff format --config "${pyproject_path}"
     run_pytest
 }
 
